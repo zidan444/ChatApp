b2e0f0c58337fae7c40f2207dcc4c75f6d343d73
zidan444 <zidanahammed444@gmail.com>
2025-12-10 14:20:02 +0530

    ch

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.use(
       // allow requests with no origin (Postman, curl)
       if(!origin) return callback(null, true);
       if(allowedOrigins.indexOf(origin) === -1){
-        return callback(new Error("Not allowed by CORS"), false);
+        return callback(new Error("Not allowed "), false);
       }
       return callback(null, true);
     },
