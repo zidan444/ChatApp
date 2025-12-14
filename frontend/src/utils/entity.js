f0b9c39786b366f77991b13b8bf9b66b544d3290
zidan444 <zidanahammed444@gmail.com>
2025-12-14 15:31:39 +0530

    sd

--- frontend/src/utils/entity.js
+++
@@ -1,9 +0,0 @@
-export const getEntityId = (entity) => {
-  if (!entity) return null;
-  if (typeof entity === "string") return entity;
-  if (typeof entity === "object") {
-    const value = entity._id || entity.id;
-    return value ? value.toString() : null;
-  }
-  return null;
-};
